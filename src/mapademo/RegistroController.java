b7b33ec73680b826a38c9fc4e88b4991c976bda5
Rafols <Rafols@DESKTOP-4UNPD1Q>
2026-05-21 11:57:10 +0200

    Initial commit

--- src/mapademo/RegistroController.java
+++ src/mapademo/RegistroController.java
@@ -4,9 +4,20 @@
  */
 package mapademo;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 
 /**
  * FXML Controller class
@@ -15,6 +26,29 @@ import javafx.fxml.Initializable;
  */
 public class RegistroController implements Initializable {
 
+    @FXML
+    private TextField CampoNickName;
+    @FXML
+    private TextField CampoCorreo;
+    @FXML
+    private PasswordField CampoContraseña;
+    @FXML
+    private Button BotonSeleccionAvatar;
+    @FXML
+    private DatePicker CampoFechaNacimiento;
+    @FXML
+    private Button BotonRegistrarse;
+    @FXML
+    private ImageView ImagenAvatar;
+    @FXML
+    private Label ErrorCorreo;
+    @FXML
+    private Label ErrorContraseña;
+    @FXML
+    private Label ErrorFechaNacimiento;
+    @FXML
+    private Label ErrorNickname;
+    private Image avatarImage = null;
     /**
      * Initializes the controller class.
      */
@@ -22,5 +56,29 @@ public class RegistroController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void SeleccionarAvatar(ActionEvent event) {
+        FileChooser chooser = new FileChooser();
+
+        chooser.setTitle("Seleccionar avatar");
+
+        chooser.getExtensionFilters().add(
+            new FileChooser.ExtensionFilter(
+                "Imágenes",
+                "*.png", "*.jpg", "*.jpeg", "*.gif"
+            )
+        );
+
+        File archivo = chooser.showOpenDialog(
+        ImagenAvatar.getScene().getWindow());
+        
+    
+    }
+
+    @FXML
+    private void Registrarse(ActionEvent event) {
+        
+    }
     
 }
